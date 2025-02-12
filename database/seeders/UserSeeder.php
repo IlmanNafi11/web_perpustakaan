8b08b93fbf64ad4c6f0fdf378fb211506009e825
IlmanNafi04 <ilmannafi04@gmail.com>
2025-02-12 16:40:44 +0700

    membuat UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table("users")->insert([
+            "name" => "ilmannafi",
+            "email"=> "masmanku03@gmail.com",
+            "password"=> bcrypt("password"),
+            "address"=> "sragen, jawa tengah",
+            "phone"=> "085123456789",
+            "photo_path"=> "default",
+            "email_verified_at" => now(),
+            "created_at" => now(),
+            "updated_at" => now(),
+            "remember_token" => Str::random(10),
+        ]);
+    }
+}
