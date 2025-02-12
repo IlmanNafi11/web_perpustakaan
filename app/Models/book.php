bfddde685bc39379b5bfaa200a7fc97d1596cea8
IlmanNafi04 <ilmannafi04@gmail.com>
2025-02-12 22:12:02 +0700

    Menambahkan relasi kemodel MediaFile

--- app/Models/book.php
+++ app/Models/book.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
+    /**
+     * Daftar atribute yang dapat diisi melalui mass-assigment.
+     * @var array
+     */
     protected $fillable = [
         "title",
         "author",
@@ -20,8 +27,14 @@ class Book extends Model
         "category_id",
     ];
 
+    /**
+     * Mendefinisikan relasi one to many dengan model MediaFile.
+     * # Setiap buku memiliki banyak file media(cover dan file ebook)
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function mediaFiles()
     {
-        // return $this->hasMany();
+        return $this->hasMany(MediaFile::class);
     }
 }
