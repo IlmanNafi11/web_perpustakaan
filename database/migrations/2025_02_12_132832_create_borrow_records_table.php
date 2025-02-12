e1df0fb75b97a80ff3fc3765b32778035c40268e
IlmanNafi04 <ilmannafi04@gmail.com>
2025-02-12 20:37:14 +0700

    migrasi table borrow record

---
+++ database/migrations/2025_02_12_132832_create_borrow_records_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('borrow_records', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('borrow_request_id')->references('id')->on('borrow_requests')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamp('borrow_at')->useCurrent();
+            $table->timestamp('return_at');
+            $table->timestamp('due_date');
+            $table->string('status')->default('borrowed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('borrow_records');
+    }
+};
