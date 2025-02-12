dd3a8df45195dfba72a1a4d816443fdaab650f2e
IlmanNafi04 <ilmannafi04@gmail.com>
2025-02-12 23:54:00 +0700

    menambahkan relasi dengan model BorrowRequest

--- app/Models/member.php
+++ app/Models/member.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    //
+    /**
+     * Daftar atribute yang dapat diisi melalui mass-assigment.
+     * @var array
+     */
+    protected $fillable = [
+        "nik",
+        "pas_foto_path",
+        "ktp_path",
+        "user_id",
+    ];
+
+    /**
+     * Mendefinisikan relasi one to one dengan model BorrowRequest
+     * # Setiap member memiliki satu permintaan peminjaman terkait.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne<BorrowRequest, Member>
+     */
+    public function borrowRequest()
+    {
+        return $this->hasOne(BorrowRequest::class);
+    }
 }
