22204e16956746b823c9da309380847e4300b17f
IlmanNafi04 <ilmannafi04@gmail.com>
2025-02-13 01:39:11 +0700

    menambahkan fillable dan relasi dengan model BorrowRequest

--- app/Models/BorrowRecord.php
+++ app/Models/BorrowRecord.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class BorrowRecord extends Model
 {
-    //
+    /**
+     * Daftar attribute yang dapat diisi melalui mass-assigment.
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        "borrow_request_id",
+        "borrow_at",
+        "return_at",
+        "due_date",
+        "status",
+    ];
+
+    /**
+     * Mendefinisikan relasi one to one dengan model BorrowRequest.
+     * Setiap record/catatan peminjaman terkait dengan satu permintaan peminjaman.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<BorrowRequest, BorrowRecord>
+     */
+    public function borrowRequests()
+    {
+        return $this->belongsTo(BorrowRequest::class);
+    }
 }
