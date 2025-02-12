9cac83bf27289d84d9748ab15b7a6bcb66f729d4
IlmanNafi04 <ilmannafi04@gmail.com>
2025-02-12 23:44:50 +0700

    menambahkan relasi dengan model Book

--- app/Models/category.php
+++ app/Models/category.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * Daftar atribute yang dapat diisi melalui mass-assigment.
+     * @var array
+     */
+    protected $fillable = [
+        "name",
+    ];
+
+    /**
+     * Mendefinisikan relasi one to one dengan model Book.
+     * # Setiap categori terkait dengan satu buku
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne<Book, Category>
+     */
+    public function book()
+    {
+        return $this->hasOne(Book::class);
+    }
 }
