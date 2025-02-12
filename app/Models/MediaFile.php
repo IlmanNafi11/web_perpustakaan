43ddeb98e37d914b8d46620424b777db8065d859
IlmanNafi04 <ilmannafi04@gmail.com>
2025-02-12 22:17:00 +0700

    menambahkan fillable dan relasi dengan model Book

--- app/Models/MediaFile.php
+++ app/Models/MediaFile.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class MediaFile extends Model
 {
-    //
+    /**
+     * Daftar atribute yang dapat diisi melalui mass-assigment.
+     * @var array
+     */
+    protected $fillable = [
+        "book_id",
+        "media_type",
+        "file_path",
+        "file_type",
+    ];
+
+    /**
+     * Mendefinisikan relasi many to one dengan model Book.
+     * # Satu atau banyak media file terkait dengan satu data buku
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
 }
