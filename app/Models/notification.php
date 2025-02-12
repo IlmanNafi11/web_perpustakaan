7736dd9780db88df535be0afee0e6ed351da41e2
IlmanNafi04 <ilmannafi04@gmail.com>
2025-02-13 00:42:54 +0700

    menambahkan relasi dengan model UserNotification

--- app/Models/notification.php
+++ app/Models/notification.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    protected $fillable = [
+        "message",
+    ];
+
+    /**
+     * Mendifinisikan relasi one to one dengan model UserNotification
+     * # Pada dasarnya model Notification memiliki relasi many to many dengan model User, model UserNotification berlaku sebagai pivot table/jembatan
+     * antara model Notification dengan model User.
+     * # Setiap notification 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne<UserNotification, Notification>
+     */
+    public function userNotification()
+    {
+        return $this->hasOne(UserNotification::class);
+    }
 }
