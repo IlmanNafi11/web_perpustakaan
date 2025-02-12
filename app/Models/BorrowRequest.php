148138b58e9bb114ebb46aa37d5296d3ee4fafd3
IlmanNafi04 <ilmannafi04@gmail.com>
2025-02-13 00:03:18 +0700

    menambahkan fillable dan relasi dengan model Member

--- app/Models/BorrowRequest.php
+++ app/Models/BorrowRequest.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class BorrowRequest extends Model
 {
-    //
+    /**
+     * Daftar atribute yang dapat diisi melalui mass-assigment.
+     * @var array
+     */
+    protected $fillable = [
+        "book_id",
+        "quantity",
+        "member_id",
+        "status",
+        "request_at",
+        "is_taken",
+    ];
+
+    /**
+     * Mendefinisikan relasi one to one dengan model Member.
+     * # Setiap permintaan peminjaman terkait dengan satu member.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<Member, BorrowRequest>
+     */
+    public function member()
+    {
+        return $this->belongsTo(Member::class);
+    }
 }
