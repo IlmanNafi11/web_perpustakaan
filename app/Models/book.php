15b77106b702f673d2f1a8832ff7f4e1ac2f8914
IlmanNafi04 <ilmannafi04@gmail.com>
2025-02-12 21:56:25 +0700

    memperbarui penamaan nama file model ke camel case

--- app/Models/book.php
+++ app/Models/book.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class book extends Model
 {
-    //
+    protected $fillable = [
+        "title",
+        "author",
+        "isbn",
+        "description",
+        "quantity",
+        "available",
+        "year",
+        "publisher",
+        "language",
+        "type",
+        "category_id",
+    ];
+
+    public function MediaFiles()
+    {
+        // return $this->hasMany();
+    }
 }
