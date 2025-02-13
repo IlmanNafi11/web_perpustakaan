3a0571d06f4641b9153448a913407cd472059178
IlmanNafi04 <ilmannafi04@gmail.com>
2025-02-14 01:35:36 +0700

    menambahkan metode afterCreate untuk insert ke table media_files

--- app/Filament/Resources/BookResource/Pages/CreateBook.php
+++ app/Filament/Resources/BookResource/Pages/CreateBook.php
@@ -3,10 +3,30 @@
 namespace App\Filament\Resources\BookResource\Pages;
 
 use App\Filament\Resources\BookResource;
+use App\Models\Book;
+use App\Models\MediaFile;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\DB;
 
 class CreateBook extends CreateRecord
 {
     protected static string $resource = BookResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function afterCreate(): void
+    {
+        $cover = $this->data['cover'];
+        $pathCover = reset($cover);
+        DB::table('media_files')->insert([
+            'book_id' => $this->record->id,
+            'media_type' => 'cover image',
+            'file_path' => $pathCover,
+        ]);
+    }
+
 }
