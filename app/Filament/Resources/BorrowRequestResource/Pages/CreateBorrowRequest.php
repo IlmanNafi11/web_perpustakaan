c4e6f7face1d637d3ebed7345d66fb21d94622d1
IlmanNafi11 <ilmannafi04@gmail.com>
2025-02-15 20:58:38 +0700

    membuat resource borrow request

---
+++ app/Filament/Resources/BorrowRequestResource/Pages/CreateBorrowRequest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\BorrowRequestResource\Pages;
+
+use App\Filament\Resources\BorrowRequestResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBorrowRequest extends CreateRecord
+{
+    protected static string $resource = BorrowRequestResource::class;
+}
