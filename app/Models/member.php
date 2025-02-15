0f7c09eac9f7000d0a9ec178eecb3bb6a25398a3
IlmanNafi11 <ilmannafi04@gmail.com>
2025-02-15 18:00:27 +0700

    menambahkan attribute tambahkan ke fillable

--- app/Models/member.php
+++ app/Models/member.php
@@ -8,7 +8,7 @@ class Member extends Model
 {
     /**
      * Daftar atribute yang dapat diisi melalui mass-assigment.
-     * 
+     *
      * @var array
      */
     protected $fillable = [
@@ -16,12 +16,17 @@ class Member extends Model
         "pas_foto_path",
         "ktp_path",
         "user_id",
+        "status",
+        "attempt_count",
+        "max_attempt",
+        "is_locked",
+        "last_attempt_at",
     ];
 
     /**
      * Mendefinisikan relasi one to one dengan model BorrowRequest
      * Setiap member memiliki satu permintaan peminjaman terkait.
-     * 
+     *
      * @return \Illuminate\Database\Eloquent\Relations\HasOne<BorrowRequest, Member>
      */
     public function borrowRequests()
@@ -32,7 +37,7 @@ class Member extends Model
     /**
      * Mendefinisikan relasi one to one dengan model User
      * Setiap member terkait dengan satu user
-     * 
+     *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<User, Member>
      */
     public function users()
