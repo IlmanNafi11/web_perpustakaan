2403e7e1e8a08c3bface993f336fc0f7735ce0ba
IlmanNafi11 <ilmannafi04@gmail.com>
2025-02-15 17:45:31 +0700

    menggunakan factory untuk seeding database

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -14,17 +15,19 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table("users")->insert([
-            "name" => "ilmannafi",
-            "email"=> "masmanku03@gmail.com",
-            "password"=> bcrypt("password"),
-            "address"=> "sragen, jawa tengah",
-            "phone"=> "085123456789",
-            "photo_path"=> "default",
-            "email_verified_at" => now(),
-            "created_at" => now(),
-            "updated_at" => now(),
-            "remember_token" => Str::random(10),
-        ]);
+        // DB::table("users")->insert([
+        //     "name" => "ilmannafi",
+        //     "email"=> "masmanku03@gmail.com",
+        //     "password"=> bcrypt("password"),
+        //     "address"=> "sragen, jawa tengah",
+        //     "phone"=> "085123456789",
+        //     "photo_path"=> "default",
+        //     "email_verified_at" => now(),
+        //     "created_at" => now(),
+        //     "updated_at" => now(),
+        //     "remember_token" => Str::random(10),
+        // ]);
+
+        User::factory(50)->create();
     }
 }
