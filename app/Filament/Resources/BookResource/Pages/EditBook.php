5e873016a53c80eb2ae88d22051daf8a2b65b690
IlmanNafi11 <ilmannafi04@gmail.com>
2025-02-15 17:39:01 +0700

    menambahkan hook afterSave untuk update data file media terkait di table media_files

--- app/Filament/Resources/BookResource/Pages/EditBook.php
+++ app/Filament/Resources/BookResource/Pages/EditBook.php
@@ -2,18 +2,27 @@
 
 namespace App\Filament\Resources\BookResource\Pages;
 
-use App\Filament\Resources\BookResource;
 use Filament\Actions;
+use Illuminate\Support\Facades\DB;
+use App\Filament\Resources\BookResource;
 use Filament\Resources\Pages\EditRecord;
+use App\Utils\HooksResource;
 
 class EditBook extends EditRecord
 {
     protected static string $resource = BookResource::class;
 
-    protected function getHeaderActions(): array
+    protected function getRedirectUrl(): string
     {
-        return [
-            Actions\DeleteAction::make(),
-        ];
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function afterSave(): void
+    {
+        HooksResource::UpdatePathFileInMediaFiles($this->data['cover'], ['book_id' => $this->record->id, 'media_type' => 'cover image']);
+
+        if (isset($this->data['ebook']) && !empty($this->data['ebook'])) {
+            HooksResource::UpdatePathFileInMediaFiles($this->data['ebook'], ['book_id' => $this->record->id, 'media_type' => 'ebook file']);
+        }
     }
 }
